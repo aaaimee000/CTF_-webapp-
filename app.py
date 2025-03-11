354312c77033e8f02afb4bd66af3e94e6a2f7aaa
aaaimee000 <86871018+aaaimee000@users.noreply.github.com>
2025-03-11 11:53:06 -0400

    add procfile & requirements

--- app.py
+++ app.py
@@ -137,5 +137,7 @@ def xss_comment():
         resp.set_cookie('flag', 'GOTCHU! Wrong way - look for the key somewhere else human.')
         return resp
 
+# if __name__ == '__main__':
+#     app.run(debug=True)
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
